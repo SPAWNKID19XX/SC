3dd9d04cad6e4ca3b96eab41934c0a7c0fb46262
boris-isac <borisisac@gmail.com>
2024-06-21 22:17:27 +0100

    [ADD] forms, setup them

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template
+from form import FormSubscribe
+from secret import SECRET_KEY
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-@app.route('/send_request_form')
-def send_request_form():
-    print(send_request_form)
+    form = FormSubscribe()
+    if form.validate_on_submit():
+        full_name = form.full_name.data
+        country = form.countries.data
+        wtsapp = form.wtsapp.data
+        return f"Thank you for subscribing, {full_name} from {country}! We will contact you at {wtsapp}."
+    return render_template('index.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
