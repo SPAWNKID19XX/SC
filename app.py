c2aa44638626f922bb783224e0eb6348f172c68e
boris-isac <borisisac@gmail.com>
2024-07-11 23:11:24 +0100

    [UPDATE] margin top of 2 last form were edited for screens < 980

--- app.py
+++ app.py
@@ -369,3 +369,9 @@ def get_couontry_code(country):
 
 if __name__ == '__main__':
     app.run(debug=False)
+
+
+
+#todo espaço de header_form  ultimas 2 formas aumentar nos screen<900px
+#todo gravar o users no disc com o nome Users_mentoria
+#todo media a partir de 900px para baixo mudar a primeira imagem de dotora
