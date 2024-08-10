befd4171034b7b0ab30c8f7daf5f746632d1dbe1
boris-isac <borisisac@gmail.com>
2024-08-10 02:25:26 +0100

    [UPDATE] the gold print html, css files

--- app.py
+++ app.py
@@ -11,7 +11,11 @@ from twilio.rest import Client
 from openpyxl import Workbook, load_workbook
 import gspread
 from google.oauth2.service_account import Credentials
+import countries
+from pythonmonkey import require as js_require
+import json
 
+countries_list = countries.countries_list
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
@@ -31,209 +35,11 @@ account_sid = my_secret_data.ACCOUNT_SID
 auth_token = my_secret_data.AUTH_TOKEN
 client = Client(account_sid, auth_token)
 
-countries_list = {
-    "Afghanistan": "+93",
-    "Albania": "+355",
-    "Algeria": "+213",
-    "Andorra": "+376",
-    "Angola": "+244",
-    "Antigua and Barbuda": "+1-268",
-    "Argentina": "+54",
-    "Armenia": "+374",
-    "Australia": "+61",
-    "Austria": "+43",
-    "Azerbaijan": "+994",
-    "Bahamas": "+1-242",
-    "Bahrain": "+973",
-    "Bangladesh": "+880",
-    "Barbados": "+1-246",
-    "Belarus": "+375",
-    "Belgium": "+32",
-    "Belize": "+501",
-    "Benin": "+229",
-    "Bhutan": "+975",
-    "Bolivia": "+591",
-    "Bosnia and Herzegovina": "+387",
-    "Botswana": "+267",
-    "Brazil": "+55",
-    "Brunei": "+673",
-    "Bulgaria": "+359",
-    "Burkina Faso": "+226",
-    "Burundi": "+257",
-    "Cabo Verde": "+238",
-    "Cambodia": "+855",
-    "Cameroon": "+237",
-    "Canada": "+1",
-    "Central African Republic": "+236",
-    "Chad": "+235",
-    "Chile": "+56",
-    "China": "+86",
-    "Colombia": "+57",
-    "Comoros": "+269",
-    "Congo, Democratic Republic of the": "+243",
-    "Congo, Republic of the": "+242",
-    "Costa Rica": "+506",
-    "Croatia": "+385",
-    "Cuba": "+53",
-    "Cyprus": "+357",
-    "Czech Republic": "+420",
-    "Denmark": "+45",
-    "Djibouti": "+253",
-    "Dominica": "+1-767",
-    "Dominican Republic": "+1-809, +1-829, +1-849",
-    "Ecuador": "+593",
-    "Egypt": "+20",
-    "El Salvador": "+503",
-    "Equatorial Guinea": "+240",
-    "Eritrea": "+291",
-    "Estonia": "+372",
-    "Eswatini": "+268",
-    "Ethiopia": "+251",
-    "Fiji": "+679",
-    "Finland": "+358",
-    "France": "+33",
-    "Gabon": "+241",
-    "Gambia": "+220",
-    "Georgia": "+995",
-    "Germany": "+49",
-    "Ghana": "+233",
-    "Greece": "+30",
-    "Grenada": "+1-473",
-    "Guatemala": "+502",
-    "Guinea": "+224",
-    "Guinea-Bissau": "+245",
-    "Guyana": "+592",
-    "Haiti": "+509",
-    "Honduras": "+504",
-    "Hungary": "+36",
-    "Iceland": "+354",
-    "India": "+91",
-    "Indonesia": "+62",
-    "Iran": "+98",
-    "Iraq": "+964",
-    "Ireland": "+353",
-    "Israel": "+972",
-    "Italy": "+39",
-    "Jamaica": "+1-876",
-    "Japan": "+81",
-    "Jordan": "+962",
-    "Kazakhstan": "+7",
-    "Kenya": "+254",
-    "Kiribati": "+686",
-    "Korea, North": "+850",
-    "Korea, South": "+82",
-    "Kosovo": "+383",
-    "Kuwait": "+965",
-    "Kyrgyzstan": "+996",
-    "Laos": "+856",
-    "Latvia": "+371",
-    "Lebanon": "+961",
-    "Lesotho": "+266",
-    "Liberia": "+231",
-    "Libya": "+218",
-    "Liechtenstein": "+423",
-    "Lithuania": "+370",
-    "Luxembourg": "+352",
-    "Madagascar": "+261",
-    "Malawi": "+265",
-    "Malaysia": "+60",
-    "Maldives": "+960",
-    "Mali": "+223",
-    "Malta": "+356",
-    "Marshall Islands": "+692",
-    "Mauritania": "+222",
-    "Mauritius": "+230",
-    "Mexico": "+52",
-    "Micronesia": "+691",
-    "Moldova": "+373",
-    "Monaco": "+377",
-    "Mongolia": "+976",
-    "Montenegro": "+382",
-    "Morocco": "+212",
-    "Mozambique": "+258",
-    "Myanmar": "+95",
-    "Namibia": "+264",
-    "Nauru": "+674",
-    "Nepal": "+977",
-    "Netherlands": "+31",
-    "New Zealand": "+64",
-    "Nicaragua": "+505",
-    "Niger": "+227",
-    "Nigeria": "+234",
-    "North Macedonia": "+389",
-    "Norway": "+47",
-    "Oman": "+968",
-    "Pakistan": "+92",
-    "Palau": "+680",
-    "Panama": "+507",
-    "Papua New Guinea": "+675",
-    "Paraguay": "+595",
-    "Peru": "+51",
-    "Philippines": "+63",
-    "Poland": "+48",
-    "Portugal": "+351",
-    "Qatar": "+974",
-    "Romania": "+40",
-    "Russia": "+7",
-    "Rwanda": "+250",
-    "Saint Kitts and Nevis": "+1-869",
-    "Saint Lucia": "+1-758",
-    "Saint Vincent and the Grenadines": "+1-784",
-    "Samoa": "+685",
-    "San Marino": "+378",
-    "Sao Tome and Principe": "+239",
-    "Saudi Arabia": "+966",
-    "Senegal": "+221",
-    "Serbia": "+381",
-    "Seychelles": "+248",
-    "Sierra Leone": "+232",
-    "Singapore": "+65",
-    "Slovakia": "+421",
-    "Slovenia": "+386",
-    "Solomon Islands": "+677",
-    "Somalia": "+252",
-    "South Africa": "+27",
-    "South Sudan": "+211",
-    "Spain": "+34",
-    "Sri Lanka": "+94",
-    "Sudan": "+249",
-    "Suriname": "+597",
-    "Sweden": "+46",
-    "Switzerland": "+41",
-    "Syria": "+963",
-    "Taiwan": "+886",
-    "Tajikistan": "+992",
-    "Tanzania": "+255",
-    "Thailand": "+66",
-    "Timor-Leste": "+670",
-    "Togo": "+228",
-    "Tonga": "+676",
-    "Trinidad and Tobago": "+1-868",
-    "Tunisia": "+216",
-    "Turkey": "+90",
-    "Turkmenistan": "+993",
-    "Tuvalu": "+688",
-    "Uganda": "+256",
-    "Ukraine": "+380",
-    "United Arab Emirates": "+971",
-    "United Kingdom": "+44",
-    "United States": "+1",
-    "Uruguay": "+598",
-    "Uzbekistan": "+998",
-    "Vanuatu": "+678",
-    "Vatican City": "+39-06",
-    "Venezuela": "+58",
-    "Vietnam": "+84",
-    "Yemen": "+967",
-    "Zambia": "+260",
-    "Zimbabwe": "+263"
-}
-
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 users_csv_file_path = os.path.join(BASE_DIR, 'users.csv')
 credentials_file_path = os.path.join(BASE_DIR, 'credentials.json')
 users_xlsx_file_path = os.path.join(BASE_DIR, 'users.xlsx')
-field_names = ['Date','Name', 'Country', 'Whatsapp']
+field_names = ['Name', 'Country', 'Whatsapp']
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -293,13 +99,21 @@ def gold_print_form():
     return render_template('form_page.html', form=form)
 
 
-def connect_and_insert_new_data_into_google_sheets(new_rec:list):
+def get_json_new_rec(new_rec:list):
+    result_json = {
+        'data_time' : new_rec[0],
+        'name' : new_rec[1],
+        'countrie' : new_rec[2],
+        'whatsapp_number' :new_rec[3],
+    }
+    #return json.dumps(result_json)
+    
     scopes = [
     "https://www.googleapis.com/auth/spreadsheets"
 ]
     creds = Credentials.from_service_account_file('credentials.json', scopes=scopes)
     client = gspread.authorize(creds)
-    SHEET_ID = "1957zv5nNuQyZ3W4DoM41ThCyJaoWQGqJArKlNiCQJkM"
+    SHEET_ID = "15buaxLcT9t8guvCbNpM_Ia-BHCuU50eTiPejPvvU2PM"
     sheet = client.open_by_key(SHEET_ID)
     values_list = sheet.sheet1.append_row(new_rec)
     
@@ -310,6 +124,7 @@ def create_exel_file_users():
     sheet.title = 'users'
     sheet.append(field_names)
     wb.save(users_xlsx_file_path)
+
 
 def is_exist_whatsapp_number(whatsapp):
     '''
@@ -324,6 +139,7 @@ def is_exist_whatsapp_number(whatsapp):
             if cell.value == whatsapp:
                 return True
     return False
+
 
 def add_new_rec_to_xlsx(new_rec_xl):
     '''
@@ -342,7 +158,7 @@ def add_new_rec_to_xlsx(new_rec_xl):
     if not is_exist_whatsapp_number(new_rec_xl[-1]):
         sheet.append(new_rec_xl)
         wb.save(users_xlsx_file_path)
-        connect_and_insert_new_data_into_google_sheets(new_rec_xl)
+        get_json_new_rec(new_rec_xl)
     else:
         print(f"{new_rec_xl[-1]} ALREADY EXISTS!!!")
         return True
@@ -376,6 +192,7 @@ def csv_to_exel():
             os.remove(users_csv_file_path)
         wb.save(users_xlsx_file_path)
 
+
 def send_email(name, country, whatsapp):
     recipient = my_secret_data.MAIL_SENDER
     subject = "New User Subscribe"
@@ -405,7 +222,6 @@ def send_msg_whatsapp(name, whatsapp):
     body=f'Hello {name}.\n we glad then you wont visit us',
     to=[f'whatsapp:{whatsapp}']
     )
-
     print(message.sid)
 
 
